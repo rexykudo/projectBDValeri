ff67364bbe680c7510be400bc419764e9560d0ad
Jonathan Demario <c14210001@john.petra.ac.id>
2022-06-28 21:03:25 +0700

    add removeMember.fxml

---
+++ coba/src/main/java/com/example/coba/DeleteController.java
@@ -0,0 +1,24 @@
+package com.example.coba;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DeleteController implements Initializable {
+    @FXML
+    private TextField idField;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
